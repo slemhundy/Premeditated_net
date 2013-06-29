274df132e95e754fd2ca49716e08d71886ae1b46
slemhundy <martin.slemmons@gmail.com>
2013-06-28 22:09:12 -0500

    adding files

--- jstyles.js
+++ jstyles.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
     //$('#selected-plays li:not(.horizontal)').addClass('sub-level');
     //$('a[href$=".pdf"]').addClass('pdflink');
     //$('a[href^="http"][href*="henry"]').addClass('henrylink');
-    $('tr:nth-child(odd)').addClass('alt');
+    $('tr:nth-child(even)').addClass('alt');
     $('a[href^="mailto:"]').addClass('mailto');
 });
